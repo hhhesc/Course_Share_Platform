0a224f04592d07849c7c9d00295d659ae463d932
hhsec <3545846249@qq.com>
2023-11-25 22:38:17 +0800

    fix_config

--- project/config/environments/development.rb
+++ project/config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  config.hosts << "21371155.project.rubyapp.act.buaa.edu.cn:9000"
 end
